c1abdf5d697112605ec4ead74e21f9289d7cab7f
GelvardMann <timoffey119@gmail.com>
2019-07-28 03:52:19 +0300

    28,07

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+
+   $('.owl-carousel').owlCarousel({
+
+      loop: true, //Зацикливаем слайдер
+
+      margin: 10, //Отступ от картино если выводите больше 1
+
+      nav: true, //Включил навигацию
+      navText: ["<i class='arrow_left'></i>",
+           "<i class='arrow_right'></i>"],
+      dots: true,
+      responsive: { //Адаптация в зависимости от разрешения экрана
+
+         0: {
+
+            items: 1
+         },
+
+         600: {
+            items: 1,
+         },
+
+         1000: {
+
+            items: 1
+
+         }
+
+      }
+   });
+});
+
+
+$(document).ready(function() {
    $('.menu_burger').click(function() {
       $('.nav_menu').slideToggle(500);
    });
@@ -6,12 +40,3 @@ $(document).ready(function() {
       $('.nav_menu').slideToggle(400);
    })
 });
-
-$(document).ready(function () {
-    //initialize swiper when document ready
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'vertical',
-      loop: true
-    })
-  });
